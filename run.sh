276ab15d33f5e365bd440d9c3cbab8b5e1fa7556
Alessandra Bilardi <alessandra.bilardi@pixartprinting.com>
2019-09-26 12:59:13 +0200

    fixed helper script

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ function help {
     echo
     echo Usage: $0 pt-command and arguments
     echo Example: $0 pt-query-digest --version
-    echo Example: $0 pt-query-digest --limit=100% tmp/slow_query.log > slow_query.pqd
+    echo "Example: $0 pt-query-digest --limit=100% tmp/slow_query.log > slow_query.pqd"
     echo WHERE slow_query.log is in the same directory of the script
     echo AND slow_query.pqd will be saved in the same directory of the script
     echo
@@ -20,10 +20,10 @@ if [ -z $1 ]; then
     help
 fi
 if [ -e Dockerfile ]; then
-    if [ $(docker images | grep -c percona-toolkit) -eq 0 ]; then
-        docker build -t local-percona-toolkit .
-    fi
     repository="local-percona-toolkit"
+    if [ $(docker images | grep -c $repository) -eq 0 ]; then
+        docker build -t $repository .
+    fi
 else
     echo If you want to use the local Dockerfile, you have to run the script in the same directory of the Dockerfile!
     docker pull $repository
