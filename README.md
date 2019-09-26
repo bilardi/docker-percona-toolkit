5f76428a1936a90c91ce62e77f55b2fdc5416505
Alessandra Bilardi <alessandra.bilardi@pixartprinting.com>
2019-09-26 12:51:59 +0200

    add documentation and helper script

--- README.md
+++ README.md
@@ -4,6 +4,24 @@
 You can run all tools included in the percona-toolkit inside a Docker container.
 See [Percona toolkit official site](https://www.percona.com/software/mysql-tools/percona-toolkit) for more detail about percona toolkit.
 
+## Usage
+If you want to use the local Dockerfile, you can run the script in the same directory of the Dockerfile
+```sh
+bash run.sh pt-query-digest --version
+```
+or you can use directly the docker commands
+```sh
+docker build -t local-percona-toolkit .
+docker run --rm -ti local-percona-toolkit pt-query-digest --version
+```
+
+If you want to use the docker image, you can run the script removing the Dockerfile (*) or
+```sh
+docker pull yuuki0xff/percona-toolkit
+docker run --rm -ti yuuki0xff/percona-toolkit pt-query-digest --version
+```
+(*) if the script doesn't find the Dockerfile, it pulls the docker image
+
 ## Included tools
 
 * pt-align
---
+++ run.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# initialization
+repository="yuuki0xff/percona-toolkit"
+
+# help
+function help {
+    echo
+    echo Usage: $0 pt-command and arguments
+    echo Example: $0 pt-query-digest --version
+    echo Example: $0 pt-query-digest --limit=100% tmp/slow_query.log > slow_query.pqd
+    echo WHERE slow_query.log is in the same directory of the script
+    echo AND slow_query.pqd will be saved in the same directory of the script
+    echo
+    exit 1
+}
+
+# main
+if [ -z $1 ]; then
+    help
+fi
+if [ -e Dockerfile ]; then
+    if [ $(docker images | grep -c percona-toolkit) -eq 0 ]; then
+        docker build -t local-percona-toolkit .
+    fi
+    repository="local-percona-toolkit"
+else
+    echo If you want to use the local Dockerfile, you have to run the script in the same directory of the Dockerfile!
+    docker pull $repository
+fi
+docker run --rm -ti -v $(pwd):/tmp:ro $repository $@
