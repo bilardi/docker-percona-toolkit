e5ab1dfd8b16b09e5b5c0cba2b07cd4b21a80980
yuuki <yuuki0xff@gmail.com>
2019-09-30 18:11:22 +0900

    improve help

--- run.sh
+++ run.sh
@@ -1,20 +1,32 @@
 #! /bin/bash
+#
+# Usage: $THIS pt-command [args]
+#
+# Run percona-toolkit commands inside docker container.  Percona tools can read
+# all files under the current directory.  You should change directory to mysql
+# log directory before execute before execute it.
+#
+# Example:
+#   cd /var/log/mysql
+#   $THIS pt-query-digest --limit=100% slow_query.log > slow_query.pqd
+#   NOTE: The slow_query.log is located in the current working directory.
+#
 # Environment:
-#   DOCKER_PERCONA_TOOLKIT: Docker image name for the percona toolkit  (default: yuuki0xff/percona-toolkit)
+#   DOCKER_PERCONA_TOOLKIT: Docker image name for the percona toolkit.
+#                           Default: yuuki0xff/percona-toolkit
+#                           Current Value: $DOCKER_PERCONA_TOOLKIT
+
 set -eu
 
 # Set default value when value is not defined.
+THIS=$0
 : "${DOCKER_PERCONA_TOOLKIT:=yuuki0xff/percona-toolkit}"
 
-# help
+# Show help
 function help {
-    echo
-    echo Usage: $0 pt-command and arguments
-    echo Example: $0 pt-query-digest --version
-    echo "Example: $0 pt-query-digest --limit=100% slow_query.log > slow_query.pqd"
-    echo WHERE slow_query.log is in the same directory of the script
-    echo AND slow_query.pqd will be saved in the same directory of the script
-    echo
+    export THIS
+    export DOCKER_PERCONA_TOOLKIT
+    envsubst <"$THIS" |sed '1,2d; /^$/Q; s/^# \?//;'
     exit 1
 }
 
